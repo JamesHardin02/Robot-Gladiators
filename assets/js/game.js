b548d3ca7bebe783566843e3baca96f03acab8d1
JamesH <jwhusc7@gmail.com>
2022-02-15 17:59:21 -0500

    added attack functionality to fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,29 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight(){
-    window.alert("The fight has begun!")
-}
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-window.prompt("What is your robot's name?");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    // subtract: enemyHealth-playerAttack
+    enemyHealth = enemyHealth - playerAttack
+    //consolelog result above
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    // subtract: playerHealth-enemyAttack
+    playerHealth = playerHealth - enemyAttack
+    // consolelog result above
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+};
 
 fight();
